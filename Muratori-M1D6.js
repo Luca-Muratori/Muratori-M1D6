968a59268e20fb725eae46fdb1265925624da21a
Lucai994 <luca.muratori.94@hotmail.it>
2021-12-06 11:00:08 +0100

    solved exercise 4-js

--- Muratori-M1D6.js
+++ Muratori-M1D6.js
@@ -158,7 +158,21 @@ console.log(splitMeString)
 /* EXERCISE 4
     Write a function called deleteOne which receives a string and a boolean as parameters.
     If the boolean value is true it should return the string without the first letter, otherwise it should remove the last one from it.
-*/
+
+    function deleteOne(str, boolean){
+    if(boolean===false){
+        strNew=str.slice(0, -1);
+    } else if (boolean===true){
+        strNew=str.substring(1);
+    }
+    return strNew
+}
+
+const deletedString=deleteOne('Hi my name is Luca', true)
+
+console.log(deletedString)
+
+    */
 
 /* EXERCISE 5
    Write a function called onlyLetters which receives a string as a parameter and returns it removing all the digits.
