59b5fc4a419a7349ed5da28fd6a528dec42176c9
Lucai994 <luca.muratori.94@hotmail.it>
2021-12-06 10:33:47 +0100

    solved exercise 2-js

--- Muratori-M1D6.js
+++ Muratori-M1D6.js
@@ -124,7 +124,20 @@ console.log(randomDice)
 
 /* EXERCISE 2
     Write a function called whoIsBigger which receives 2 numbers as parameters and returns the biggest one.
-*/
+
+    function compare(num1, num2) {
+	if (num1>num2) {
+		return num1 + ' is the biggest number';
+	} else if (num2 > num1) {
+		return num2 + ' is the biggest number';
+	} else if (num1===num2) {
+		return 'The numbers are equal';
+	}
+}
+let results = compare(10, 20);
+console.log(results);
+
+    */
 
 /* EXERCISE 3
     Write a function called splitMe which receives a string as a parameter and returns an array with every word in that string.
--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-/* EXERCISE 1
-    Write a function called dice; it should
-     randomize an integer number between 1 and 6.
+/* EXERCISE 2
+    Write a function called whoIsBigger which receives 2 numbers
+     as parameters and returns the biggest one.
 */
 
-function dice(){
-    num=Math.round(Math.random()*6)
-        for (i = 1; i < 6; i++) {
-            num = Math.round(Math.random()*6)+1;
-        return num
-        }
+function compare(num1, num2) {
+	if (num1>num2) {
+		return num1 + ' is the biggest number';
+	} else if (num2 > num1) {
+		return num2 + ' is the biggest number';
+	} else if (num1===num2) {
+		return 'The numbers are equal';
+	}
 }
-const randomDice=dice()
-console.log(randomDice)
+let results = compare(10, 20);
+console.log(results);
