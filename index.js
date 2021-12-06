0df83e9d6fd2a4e96aa8348f6f9d4e42d64d35d7
Lucai994 <luca.muratori.94@hotmail.it>
2021-12-06 12:13:11 +0100

    solved exercise 7-js

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-
-
-/* EXERCISE 6
-   Write a function called isThisAnEmail which receives a string
- as a parameter and returns true if the string is a valid email address.
+/* EXERCISE 7
+   Write a function called whatDayIsIt
+    that should return the current day of the week.
 */
 
+const whatDayIsIt =function(input){
+    const day = new Date(input);
+    const day1 =day.getDay();
+    
+    return  (day1===1) ? console.log(`The ${input} is a Monday`)
+        :(day1===2)? console.log(`The ${input} is a Tuesday`)
+        :(day1===3)? console.log(`The ${input} is a Wednesday`)
+        :(day1===4)? console.log(`The ${input} is a Thursday`)
+        :(day1===5)? console.log(`The ${input} is a Friday`)
+        :(day1===6)? console.log(`The ${input} is a Saturday`)
+        :console.log(`The ${input} is a Sunday`);
+    
 
-const isThisAnEmail= function(input) {
 
-    let validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-  
-    if (input.match(validRegex)) {
-  
-      console.log(`${input} is a valid email address!"`);
+}
 
-    } else {
-  
-        console.log(`${input} is an invalid email address!`);
-
-    }
-  
-  };
-
-  isThisAnEmail('luca.muratoriotmail.it');
-  isThisAnEmail('luca.muratori@hotmail.it');
- 
-  
+whatDayIsIt('12/05/2021')
+whatDayIsIt('12/04/2021')
+whatDayIsIt('12/03/2021')
+whatDayIsIt('12/02/2021')
+whatDayIsIt('12/01/2021')
+whatDayIsIt('11/30/2021')
+whatDayIsIt('11/29/2021')
