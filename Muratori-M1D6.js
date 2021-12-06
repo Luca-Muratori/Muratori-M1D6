4dd923be88e6c94bc071cd971d1aae038a6f84b3
Lucai994 <luca.muratori.94@hotmail.it>
2021-12-06 11:13:55 +0100

    solved exercise 5-js

--- Muratori-M1D6.js
+++ Muratori-M1D6.js
@@ -177,6 +177,17 @@ console.log(deletedString)
 /* EXERCISE 5
    Write a function called onlyLetters which receives a string as a parameter and returns it removing all the digits.
    Ex.: onlyLetters("I have 4 dogs") => returns "I have  dogs"
+
+   function onlyLetters(string){
+    
+    let withNoDigits = string.replace(/[0-9]/g, '');
+    
+    return withNoDigits
+    
+}
+
+let splitMeString = onlyLetters("I have 4 dogs")
+console.log(splitMeString)
 */
 
 /* EXERCISE 6
--- index.js
+++ index.js
@@ -1,19 +1,16 @@
-/* EXERCISE 4
-    Write a function called deleteOne which receives
-     a string and a boolean as parameters.
-    If the boolean value is true it should return the string
-     without the first letter, otherwise it should remove the last one from it.
+/* EXERCISE 5
+   Write a function called onlyLetters which receives a string
+    as a parameter and returns it removing all the digits.
+   Ex.: onlyLetters("I have 4 dogs") => returns "I have  dogs"
 */
 
-function deleteOne(str, boolean){
-    if(boolean===false){
-        strNew=str.slice(0, -1);
-    } else if (boolean===true){
-        strNew=str.substring(1);
-    }
-    return strNew
+function onlyLetters(string){
+    
+    let withNoDigits = string.replace(/[0-9]/g, '');
+    
+    return withNoDigits
+    
 }
 
-const deletedString=deleteOne('Hi my name is Luca', true)
-
-console.log(deletedString)
+let splitMeString = onlyLetters("I have 4 dogs")
+console.log(splitMeString)
