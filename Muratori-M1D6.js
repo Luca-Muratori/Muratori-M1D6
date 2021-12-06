db0960c80dcf1207680d93b571ca0ad654388e38
Lucai994 <luca.muratori.94@hotmail.it>
2021-12-06 09:35:37 +0100

    exercise  C-solved

--- Muratori-M1D6.js
+++ Muratori-M1D6.js
@@ -30,6 +30,17 @@ console.log(result)
 
 /* EXERCISE C
     Create a variable called random and assign to it a random number between 0 and 20 (it should be randomly created at each execution).
+
+function casualNumber (){
+    num=Math.round(Math.random()*20)
+        for (i = 1; i < 20; i++) {
+            num = Math.round(Math.random()*20);
+    return num
+        }
+}
+const random=casualNumber()
+console.log(`You're thinking of ${random}`)
+
 */
 
 /* EXERCISE D
--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 
 
-function letSum(a ,b){
-    return a+b
-}
-const result =letSum(1,3)
+/*EXERCISE C
+Create a variable called random and assign to it a random number 
+between 0 and 20 (it should be randomly created at each execution).*/
 
-console.log(result)
+function casualNumber (){
+    num=Math.round(Math.random()*20)
+        for (i = 1; i < 20; i++) {
+            num = Math.round(Math.random()*20);
+    return num
+        }
+}
+const random=casualNumber()
+console.log(`You're thinking of ${random}`)
+
+
+
